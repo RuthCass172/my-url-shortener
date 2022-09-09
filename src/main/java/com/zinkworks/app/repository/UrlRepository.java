c8bc823a064cd001385850f675f913fb4b57e6a4
RuthCass172 <ruth.cassidy@zinkworks.com>
2022-09-09 10:25:35 +0100

    Add comments to the repository and change ID type to String

--- src/main/java/com/zinkworks/app/repository/UrlRepository.java
+++ src/main/java/com/zinkworks/app/repository/UrlRepository.java
@@ -4,6 +4,9 @@ import com.zinkworks.app.model.Url;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/* The two parameters passed are <T, ID> the T refers to the
+   entity/model that the repository will manage and ID refers to
+   the type of the ID field */
 @Repository
-public interface UrlRepository extends JpaRepository<Url, Long> {
+public interface UrlRepository extends JpaRepository<Url, String> {
 }
